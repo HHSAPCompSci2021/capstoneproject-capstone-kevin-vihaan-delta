88e6e6b7158cbdfb18bac6d3668d8661f76fcb13
Vihaan Chinthakindi <vchinthakindi876@student.fuhsd.org>
2022-05-03 10:10:12 -0700

    added drawingsurface/papplet

---
+++ CapstoneProject/src/DrawingSurface.java
@@ -0,0 +1,28 @@
+
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.geom.Point2D;
+
+
+import processing.core.PApplet;
+
+public class DrawingSurface {
+//	private Level board;
+	
+	public DrawingSurface() {
+		//board = new Level("testfiles.txt");
+	}
+	
+	
+	public void draw() { 
+		//background(255);   
+		//fill(0);
+		//textAlign(LEFT);
+		//textSize(12);
+		
+	//	if (board != null) {
+	//		board.draw(this, 0, 0, height, height);
+	//	}
+		
+	}
+}
