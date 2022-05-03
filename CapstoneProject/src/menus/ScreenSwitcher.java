69d945f02821a913ce967bec0ad73956c70cfcf9
Vihaan Chinthakindi <vchinthakindi876@student.fuhsd.org>
2022-05-03 10:43:51 -0700

    added screen classes

---
+++ CapstoneProject/src/menus/ScreenSwitcher.java
@@ -0,0 +1,11 @@
+package menus;
+
+
+public interface ScreenSwitcher {
+	public static final int MENU_SCREEN = 0;
+	public static final int GAME_SCREEN = 1;
+	public static final int SHOP_SCREEN = 2;
+	
+	public static final int LEVELCHOOSER_SCREEN = 3;
+	public void switchScreen(int i);
+}
