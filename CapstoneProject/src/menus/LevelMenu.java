d3c38f70c7097937ea93618e88d97602a5a14567
Vihaan Chinthakindi <vchinthakindi876@student.fuhsd.org>
2022-05-05 17:40:06 -0700

    level1/level2 buttons

--- CapstoneProject/src/menus/LevelMenu.java
+++ CapstoneProject/src/menus/LevelMenu.java
@@ -5,28 +5,38 @@ import java.awt.Rectangle;
 import core.DrawingSurface;
 
 public class LevelMenu extends Screen {
-	
-	
+
 	private DrawingSurface surface;
-	
 
 	private Rectangle screenRect;
-	
-	
-	
+	private Rectangle level1;
+	private Rectangle level2;
+
 	public LevelMenu(DrawingSurface surface) {
 		super(800, 800);
 		this.surface = surface;
-		screenRect = new Rectangle(0,0,DRAWING_WIDTH,DRAWING_HEIGHT);
-		
+		screenRect = new Rectangle(0, 0, DRAWING_WIDTH, DRAWING_HEIGHT);
+		level2 = new Rectangle(300, 100, 100, 100);
+		level1 = new Rectangle(100, 100, 100, 100);
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	
-	public void draw()
-	{
-		surface.background(0,255,255);   
-		surface.rect(100,100,100,100);
+	public void draw() {
+		surface.background(0, 255, 255);
+		surface.rect(level1.x, level1.y, level1.width, level1.height, 10, 10, 10, 10);
+
+		surface.fill(0);
+		String str = "Level 1";
+		float w = surface.textWidth(str);
+		surface.textSize(30);
+		surface.text(str, level1.x + level1.width / 2 - w / 2, level1.y + level1.height / 2);
+		surface.fill(255);
+		surface.rect(level2.x, level2.y, level2.width, level2.height, 10, 10, 10, 10);
+		surface.fill(0);
+		String str1 = "Level 2";
+		float w1 = surface.textWidth(str);
+		surface.textSize(30);
+		surface.text(str1, level2.x + level2.width / 2 - w1 / 2, level2.y + level2.height / 2);
+
 	}
 }
