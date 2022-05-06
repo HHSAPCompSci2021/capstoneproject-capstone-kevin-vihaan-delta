fabb060bf1ff793aedc4360d8bcdf575ca95e812
Kevin Valencia <kvalenciaespitia426@student.fuhsd.org>
2022-05-06 10:26:34 -0700

    update levelone

--- CapstoneProject/src/levels/LevelOne.java
+++ CapstoneProject/src/levels/LevelOne.java
@@ -1,5 +1,46 @@
 package levels;
 
-public class LevelOne {
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import core.DrawingSurface;
+import core.Player;
+import menus.Screen;
+import obstacles.Obstacle;
+import obstacles.Wall;
+
+
+
+
+public class LevelOne extends Screen {
+
+	private DrawingSurface surface;
+	
+	private Rectangle screenRect;
+
+	private Player user;
+	private List<Obstacle> obstacles;
+	
+	
+	public LevelOne(DrawingSurface surface) {
+		super(800, 600);
+		
+		this.surface = surface;
+		screenRect = new Rectangle(0,0,DRAWING_WIDTH,DRAWING_HEIGHT);
+		obstacles = new ArrayList<Obstacle>();
+		obstacles.add(new Wall());
+		
+		
+		
+	}
+
+
+	
+	
+	
+	
+	
 }
