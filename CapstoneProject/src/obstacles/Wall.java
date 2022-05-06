0d3d89168f3e8bc8ce9ebbb0c2b6bf7a7ec41e86
Vihaan Chinthakindi <vchinthakindi876@student.fuhsd.org>
2022-05-06 10:21:50 -0700

    javadoc

--- CapstoneProject/src/obstacles/Wall.java
+++ CapstoneProject/src/obstacles/Wall.java
@@ -1,5 +1,9 @@
 package obstacles;
-
+/**
+ * 
+ * @author vihaanchinthakindi and kevinvalencia
+ * Class Represents the Walls in each level that player cannot pass
+ */
 public class Wall extends Obstacle{
 
 }
