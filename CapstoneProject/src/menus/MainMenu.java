38a556955b8e5737378a2643e4b386157906ae48
Vihaan Chinthakindi <vchinthakindi876@student.fuhsd.org>
2022-05-15 00:08:47 -0700

    image loads in for mainmenu

--- CapstoneProject/src/menus/MainMenu.java
+++ CapstoneProject/src/menus/MainMenu.java
@@ -2,6 +2,7 @@ package menus;
 import java.awt.Point;
 import java.awt.Rectangle;
 import processing.core.PApplet;
+import processing.core.PImage;
 import core.DrawingSurface;
 import menus.ScreenSwitcher;
 
@@ -17,6 +18,7 @@ public class MainMenu extends Screen {
 	private Rectangle chooseLevelButton;
 	private Rectangle shopButton;
 	private Rectangle quitButton;
+	private PImage image;
 	/**
 	 * 
 	 * @param surface takes in a DrawingSurface to perform drawing with
@@ -28,14 +30,25 @@ public class MainMenu extends Screen {
 		shopButton = new Rectangle(800/2-100,800/2-50,200,100);
 		quitButton = new Rectangle(800/2-100,800/2+100,200,100);
 		chooseLevelButton = new Rectangle(800/2-100,800/2-200,200,100);
+
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setup() {
+		
+		image = surface.loadImage("img/placeholder.png");
+		image.resize(surface.width,surface.height);
+		
 	}
 	/**
 	 * Draws the MainMenu with  buttons indicating the ShopMenu, LevelMenu, and quit button
 	 */
 	public void draw() {
 
-		surface.background(255,255,255);
+		//surface.background(255,255,255);
+		surface.background(image);
+		//surface.background
+	
 		
 		surface.rect(shopButton.x, shopButton.y, shopButton.width, shopButton.height, 10, 10, 10, 10);
 		surface.fill(0);
