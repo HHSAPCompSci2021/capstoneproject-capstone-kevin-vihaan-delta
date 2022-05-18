c08b7758495dff8ec30bf200b0357ea1bb75d4c8
Vihaan Chinthakindi <vchinthakindi876@student.fuhsd.org>
2022-05-17 20:04:57 -0700

    MainMenu works

--- CapstoneProject/src/menus/MainMenu.java
+++ CapstoneProject/src/menus/MainMenu.java
@@ -30,10 +30,9 @@ public class MainMenu extends Screen {
 		super(800, 800);
 		this.surface = surface;
 
-		shopButton = new Rectangle(DRAWING_HEIGHT / 2 - (DRAWING_HEIGHT / 8),
-				DRAWING_HEIGHT / 2 - (DRAWING_HEIGHT / 16), DRAWING_HEIGHT / 4, DRAWING_HEIGHT / 8);
-		quitButton = new Rectangle(800 / 2 - 100, 800 / 2 + 100, 200, 100);
-		chooseLevelButton = new Rectangle(300,200,200,100);
+		shopButton = new Rectangle(300, 350, 200, 100);
+		quitButton = new Rectangle(300, 500, 200, 100);
+		chooseLevelButton = new Rectangle(300, 200, 200, 100);
 
 		// TODO Auto-generated constructor stub
 	}
@@ -58,30 +57,28 @@ public class MainMenu extends Screen {
 		super.draw();
 		surface.fill(255);
 
-		surface.text("Press ESC to go back to Main Menu.", 20, 90);
-		surface.text("Press 1 to Rotate ", 20, 60);
-		surface.rect(shopButton.x, shopButton.y, shopButton.width, shopButton.height, 10, 10, 10, 10);
+		surface.text("Press ESC to go back to Main Menu.", -380, -310);
+		surface.text("Press 1 to Rotate ", -380, -340);
+		surface.rect(-100, -50, shopButton.width, shopButton.height, 10, 10, 10, 10);
 		surface.fill(0);
 		String str = "Shop";
 		float w = surface.textWidth(str);
 		surface.textSize(30);
-		surface.text(str, shopButton.x + shopButton.width / 2 - w / 2, shopButton.y + shopButton.height / 2);
+		surface.text(str, -100 + shopButton.width / 2 - w / 2, -50 + shopButton.height / 2);
 
 		surface.fill(255);
-		surface.rect(quitButton.x, quitButton.y, quitButton.width, quitButton.height, 10, 10, 10, 10);
+		surface.rect(-100, 100, quitButton.width, quitButton.height, 10, 10, 10, 10);
 		surface.fill(0);
 		String str2 = "Quit";
 		float x = surface.textWidth(str2);
-		surface.text(str2, quitButton.x + quitButton.width / 2 - x / 2, quitButton.y + quitButton.height / 2);
+		surface.text(str2, -100 + quitButton.width / 2 - x / 2, 100 + quitButton.height / 2);
 
 		surface.fill(255);
-		surface.rect(-100, -200, 200, 100, 10,
-				10, 10, 10);
+		surface.rect(-100, -200, 200, 100, 10, 10, 10, 10);
 		surface.fill(0);
 		String str3 = "Choose \nLevel";
 		float y = surface.textWidth(str3);
-		surface.text(str3, -100 + 200 / 2 - y / 2,
-				-200 + 100 / 2);
+		surface.text(str3, -100 + 200 / 2 - y / 2, -200 + 100 / 2);
 
 	}
 
@@ -91,7 +88,7 @@ public class MainMenu extends Screen {
 	public void mousePressed() {
 		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX, surface.mouseY));
 		if (chooseLevelButton.contains(p)) {
-			//System.out.println("HI");
+			// System.out.println("HI");
 			surface.switchScreen(ScreenSwitcher.LEVELCHOOSER_SCREEN);
 		}
 		if (shopButton.contains(p)) {
